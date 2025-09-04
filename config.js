7331b377110695d2bdcf74b8bd656cd571300204
darako0409 <166584884+darako0409@users.noreply.github.com>
2025-09-04 14:38:38 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1as9zQr8P6k8Up9Y4FnO3DYroyvgMnAM",
+  authDomain: "karennda-e8465.firebaseapp.com",
+  projectId: "karennda-e8465",
+  storageBucket: "karennda-e8465.firebasestorage.app",
+  messagingSenderId: "1023443880200",
+  appId: "1:1023443880200:web:ee995edd613fbc626544bd",
+  measurementId: "G-E9Y0L9P95V"
+};
+
+export { firebaseConfig };
